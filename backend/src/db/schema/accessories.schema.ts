56e0074811f53d7988b9e91fae25f18b96650ced
DGLOliveira <dgloliveira90@gmail.com>
2026-07-12 19:00:05 +0000

    Refactored accessories schema in order to allow for product groupings based on a groupings enum table

--- backend/src/db/schema/accessories.schema.ts
+++ backend/src/db/schema/accessories.schema.ts
@@ -1,10 +1,12 @@
-import { integer, pgTable, varchar, timestamp, text, decimal, smallint, unique, boolean } from "drizzle-orm/pg-core";
+import { integer, pgTable, pgEnum, varchar, timestamp, text, decimal, smallint, unique, boolean } from "drizzle-orm/pg-core";
 import { brandsTable } from "./brands.schema.ts";
 
 const timestamps = {
     updated_at: timestamp(),
     created_at: timestamp().defaultNow().notNull(),
 };
+
+export const groupings = pgEnum("grouping", ["Standalone", "Pieces", "Color", "Weight", "Qualitative"]);
 
 export const accessoriesCategoryTable = pgTable(
     "accessories_categories", 
@@ -29,10 +31,13 @@ export const accessoriesTable = pgTable(
     "accessories", 
     {
     id: integer().primaryKey().generatedAlwaysAsIdentity(),
-    name: varchar({ length: 255 }).unique().notNull(),
+    name: varchar({ length: 255 }).notNull(), //If several accessories have the same name, they will be grouped
+    subname: varchar({ length: 255 }),
+    grouping: groupings().notNull(),
     brandId: integer("brand_id").references(() => brandsTable.id).notNull(),
-    subcategoryId: integer("sub_category_id").references(() => accessoriesSubCategoryTable.id).notNull(),
-    price: decimal({ precision: 6, scale: 2 }),
+    categoryId: integer("category_id").references(() => accessoriesCategoryTable.id).notNull(),
+    subcategoryId: integer("sub_category_id").references(() => accessoriesSubCategoryTable.id),
+    price: decimal({ precision: 6, scale: 2 }).notNull(),
     discount: smallint().notNull(),
     stock: smallint().notNull(),
     description: text(),
@@ -41,18 +46,18 @@ export const accessoriesTable = pgTable(
     }
 );
 
+
 export const accessoriesImagesTable = pgTable(
     "accessories_images", 
     {
     id: integer().primaryKey().generatedAlwaysAsIdentity(),
     name: varchar({ length: 255 }).unique().notNull(),
     accessoryId: integer("accessory_id").references(() => accessoriesTable.id).notNull(),
-    image_url: varchar({ length: 255 }).unique().notNull(), //Image url is on an external service
+    image_url: varchar({ length: 255 }).notNull(), //Image url is on an external service
     index: smallint().notNull(),
-    is_thumbnail: boolean().notNull(), //Determines if the image is the preview image used for the store list
     ...timestamps
     },
     (table) => [
-        unique().on(table.id, table.image_url), //Prevent duplicate images
+        unique().on(table.accessoryId, table.image_url), //Prevent duplicate images per accessory
     ]
 )
